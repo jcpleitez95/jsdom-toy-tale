ae7e58571314bf31d7960cc15ceb7e8762207cd7
jcpleitez95 <jcpleitez@outlook.com>
2020-12-17 14:05:54 -0500

    Done 2.0

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
     addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
+      // added event listener to form so that it would post new toy 
       toyFormContainer.addEventListener('submit', (event)=>{
       event.preventDefault()
       postToy(event.target)
@@ -19,19 +20,16 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+// grabbing data from db.json and render onto page
 fetch("http://localhost:3000/toys")
-.then(response => {
-return response.json()
-}).then ((toyArray) => {
-  // console.log(toyObj)
+.then(response => response.json())
+.then ((toyArray) => {
   toyArray.forEach(toy =>{
     renderToys(toy)
   })
 })
 
-
-
-
+// post request for information typed into form and then renders onto page
 function postToy(data){
   const toyName = data.name.value
   const imgUrl = data.image.value
@@ -50,16 +48,16 @@ function postToy(data){
   })
     .then(response => response.json())
     .then((toyObj) => {
-      // console.log(toyObj)
       let newToy = renderToys(toyObj)
       toyCollection.append(newToy)
     })
 }
 
+// patch request for likes 
 function likeToy(event) {
   event.preventDefault()
   let moreLikes = parseInt(event.target.previousElementSibling.innerText) + 1
-  console.log(event)
+           
   fetch(`http://localhost:3000/toys/${event.target.id}`, {
     method: 'PATCH', 
     headers: {
@@ -74,16 +72,15 @@ function likeToy(event) {
     }))
 }
 
+// renders toys onto screen
 function renderToys(toy) {
   const div = document.createElement("div")
   div.classList.add("card")
   div.dataset.id = toy.id
-  // element.classList.add("mystyle")
   
   let h2 = document.createElement('h2')
   h2.innerText = toy.name
   
-
   let img = document.createElement('img')
   img.src = toy.image
   img.alt = `${toy.name}`
